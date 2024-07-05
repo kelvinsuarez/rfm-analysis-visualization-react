e087ebdfa5aa236444f8b469a868e952a5fc0536
Kelvin Suarez <k_suarez22@hotmail.com>
2024-07-05 06:00:26 -0700

    creando componente Main

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import { Bar } from 'react-chartjs-2';
 import Papa from 'papaparse';
 import Header from './Header';
-import TableauDashboard from './TableauDashboard';
+import Main from './Main';
 import Footer from './Footer';
 import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 
@@ -94,50 +93,13 @@ function App() {
     <div className="App">
       <div className="root">
         <Header/>
-        <main>
-          <section id="upload-section">
-            <h2>Modos de carga</h2>
-            <button onClick={loadData}>Cargar Datos</button>
-            <div className="file-upload">
-              <label htmlFor="file-input" className="file-upload-label">
-                Seleccionar Archivo
-              </label>
-              <input id="file-input" type="file" accept=".json,.csv" onChange={handleFileUpload} className="file-upload-input" />
-              <span className="file-upload-name">{fileName}</span>
-            </div>
-            <TableauDashboard url={url} />
-          </section>
-          <section id="charts-section">
-            <h2>Visualización de Datos</h2>
-            <div style={{ width: '100%' }}> {/* Ajuste para que ocupe el ancho completo */}
-              {chartData && (
-                <Bar
-                  data={chartData}
-                  options={{
-                    indexAxis: 'y',
-                    elements: {
-                      bar: {
-                        borderWidth: 2,
-                      },
-                    },
-                    responsive: true,
-                    plugins: {
-                      legend: {
-                        position: 'right',
-                      },
-                      title: {
-                        display: true,
-                        text: 'Gráfico de Barras',
-                      },
-                    },
-                  }}
-                />
-              )}
-            </div>
-            <div>
-            </div>
-          </section>
-        </main>
+        <Main
+          loadData={loadData}
+          handleFileUpload={handleFileUpload}
+          fileName={fileName}
+          chartData={chartData}
+          url={url}
+        />
         <Footer/>
       </div>
     </div>
